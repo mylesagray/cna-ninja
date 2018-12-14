fe1747cf1ba7f9f4b10d6a898a49d962656ae182
Myles Gray <mg@mylesgray.com>
2018-12-14 16:44:46 +0000

    Added Part 0 and initial v3 content

---
+++ 00_Tools/readme.md
@@ -0,0 +1,7 @@
+Explain the kitchen
+
+VS Code
+POSTman
+iTerm2
+ZSH
+Brew install packages
---
+++ 02_Scaling-Out/v3/src/app.py
@@ -0,0 +1,18 @@
+# Import necessary libraries
+import sys
+import qrcode
+import os
+
+# Set the name of the output directory for the QRCode
+directory = "output"
+
+# Create directory if it doesn't exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Create a QRCode from the input query of the application and save to disk
+img = qrcode.make(sys.argv[1])
+img.save(directory+"/"+sys.argv[1]+".png", "PNG")
+
+# Output confirmation text to the CLIcd Gi  
+print("Created QR Code for: " + sys.argv[1])
